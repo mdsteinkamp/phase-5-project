716258a31c3862a6c2d177cac4212d33bea12f9e
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-11 21:41:23 -0800

    added signup form to signup comp

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,87 @@
 import { StyledLogin } from "./styles/Login.styled"
+import { useState, useContext } from "react"
+import { UserContext } from "./UserContext"
+import { useNavigate } from "react-router-dom"
 
 export default function Signup() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    passwordConfirmation: ""
+  })
+  const [errors, setErrors] = useState([])
+  const {setUser} = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  function handleChange(e) {
+    const name = e.target.name
+    const value = e.target.value
+    setFormData({
+      ...formData,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+    .then((resp) => {
+      if (resp.ok) {
+        resp.json().then((user) => setUser(user))
+        navigate("/chords")
+      } else {
+        resp.json().then(e => {
+          setErrors(e.errors)
+          console.log(errors)
+        })
+      }
+    });
+  }
 
   return (
     <StyledLogin>
-      <h1>Create Account</h1>
+      <h1>Signup Here</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Confirm Password"
+            value={formData.passwordConfirmation}
+            onChange={handleChange}
+          />
+          <br />
+          <button>Signup</button>
+        </form>
+        {errors.length > 0 &&
+            <ul>{errors.map(e => (
+              <ul key={e}>
+                <h3>{e}</h3>
+              </ul>))}
+            </ul>
+          }
     </StyledLogin>
   )
 }
