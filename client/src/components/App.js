f8458c1d24f0ef25ab057a8d4dc6449dd3010668
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-11 17:56:27 -0800

    Configured client-side routing

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+import { Routes, Route } from "react-router-dom"
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count))
+  }, [])
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/testing" element={<h1>TestRoute</h1>}>
+        </Route>
+        <Route path="/" element={<h1>Page Count: {count}</h1>}>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
