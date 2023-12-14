704143f422db1ff02bbc6ac70cd77b53d4fdef75
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-13 18:09:21 -0800

    sessions controller updated to include pantry_items & related foodstuffs in json on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: user, status: :created
+      render json: user, status: :created, include: ['pantry_items', 'pantry_items.foodstuff']
     else
       render json: { errors: ["Invalid username or password"] }, status: :unauthorized
     end
