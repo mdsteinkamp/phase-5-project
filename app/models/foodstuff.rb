125be94351150cd502e747b602606ad91cacaaa2
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-15 17:29:17 -0800

    added uniquness validation for foodstuff name

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -2,4 +2,6 @@ class Foodstuff < ApplicationRecord
   has_many :pantry_items
   has_many :users, through: :pantry_items
   validates :name, :unit, :category, presence: true
+  validates :name, uniqueness: true
+
 end
