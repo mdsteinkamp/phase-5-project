a65f201b1a0b97379bc269d2a0ea8477a6523ee4
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-28 21:39:42 -0800

    updated logic for checking makeable recipes in AvailableRecipesPage, needs to work if ingredient is NOT in pantry list

--- client/src/components/AvailableRecipesPage.js
+++ client/src/components/AvailableRecipesPage.js
@@ -1,11 +1,12 @@
 import { StyledUserPantry } from "./styles/UserPantry.styled"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "./UserContext"
 import { RecipesContext } from "./RecipesContext"
 
 export default function AvailableRecipesPage() {
   const {recipes} = useContext(RecipesContext)
   const {user} = useContext(UserContext)
+	const [makeableRecipes, setMakeableRecipes] = useState([])
     
   if (!recipes, !user) return(<h1>Loading data...</h1>)
 	console.log(recipes, user)
@@ -15,24 +16,29 @@ export default function AvailableRecipesPage() {
 		recipes: []
 	}
 	let recipesToSearch = [...recipes]
-	const makeableRecipes = user.pantry_items.map(item => {
-		for (const recipe of recipesToSearch) {
-			if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) {
-				break
-			} else if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity > item.quantity)) {
-				console.log(recipe)
-				console.log(recipe.name, arr)
-				arr = arr.filter(r => r.name !== recipe.name)
-				recipesToSearch = recipesToSearch.filter(r => r.name !== recipe.name)
-				return recipesToSearch
+
+  function handleCheckIngredients() {
+    const results = new Set()
+		const unmakeableRecipes = []
+		for (const recipe of recipes)
+			for (const ingredient of recipe.ingredients) {
+				if (user.pantry_items.find(i => i.foodstuff.name === ingredient.foodstuff.name && ingredient.quantity > i.quantity)) {
+					unmakeableRecipes.push(recipe.name)
+				}	if ((user.pantry_items.find(i => i.foodstuff.name === ingredient.foodstuff.name && ingredient.quantity <= i.quantity))) {
+					results.add(recipe)
+				} 
 			}
+		console.log(unmakeableRecipes)
+		let resultsArray = [...results]
+		for (const recipe of unmakeableRecipes) {
+			resultsArray = resultsArray.filter(r => r.name !== recipe)
+		}
+		console.log(resultsArray)
 
-		} 
-		return recipes
-		
-	})
+  }
+	handleCheckIngredients()
 
-	console.log(makeableRecipes)
+	// console.log(makeableRecipes.pop())
 
 
 	return (
@@ -42,6 +48,26 @@ export default function AvailableRecipesPage() {
 
 	)
 }
+
+// const makeableRecipes = user.pantry_items.map(item => {
+// 	for (const recipe of recipesToSearch) {
+// 		 if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) {
+// 			console.log(recipesToSearch)
+// 			break
+// 		} else if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name !== item.foodstuff.name)) {
+// 			console.log("can't make this")
+// 			recipesToSearch = recipesToSearch.filter(r => r.name !== recipe.name)
+// 		} else if (recipe.ingredients.find(ingredient => (ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity > item.quantity))) {
+// 			console.log(recipe)
+// 			console.log(recipe.name, arr)
+// 			arr = arr.filter(r => r.name !== recipe.name)
+// 			recipesToSearch = recipesToSearch.filter(r => r.name !== recipe.name)
+// 			return recipesToSearch
+// 		}
+
+// 	} 
+// 	return recipes
+// })
 
 // const makeableRecipes = user.pantry_items.map(item => {
 // 	const arr = []
