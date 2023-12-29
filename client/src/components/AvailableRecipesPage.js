ddc11ca93d2dddc81aa7901475d4c2deb32493ea
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-12-28 20:39:18 -0800

    AvailableRecipes algo tentatively working in AvailableRecipesPage

--- client/src/components/AvailableRecipesPage.js
+++ client/src/components/AvailableRecipesPage.js
@@ -10,14 +10,25 @@ export default function AvailableRecipesPage() {
   if (!recipes, !user) return(<h1>Loading data...</h1>)
 	console.log(recipes, user)
 
+	let arr = []
+	let obj = {
+		recipes: []
+	}
+	let recipesToSearch = [...recipes]
 	const makeableRecipes = user.pantry_items.map(item => {
-		const arr = []
-		for (const recipe of recipes) {
-			if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) { arr.push(recipe.name, item.foodstuff.name)}
+		for (const recipe of recipesToSearch) {
+			if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) {
+				break
+			} else if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity > item.quantity)) {
+				console.log(recipe)
+				console.log(recipe.name, arr)
+				arr = arr.filter(r => r.name !== recipe.name)
+				recipesToSearch = recipesToSearch.filter(r => r.name !== recipe.name)
+				return recipesToSearch
+			}
 
-		}
-
-		return arr
+		} 
+		return recipes
 		
 	})
 
@@ -31,3 +42,25 @@ export default function AvailableRecipesPage() {
 
 	)
 }
+
+// const makeableRecipes = user.pantry_items.map(item => {
+// 	const arr = []
+// 	for (const recipe of recipes) {
+// 		if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) { arr.push(recipe.name, item.foodstuff.name)}
+
+// 	}
+
+// 	return arr
+	
+// })
+
+// const set = new Set()
+// const makeableRecipes = user.pantry_items.map(item => {
+// 	for (const recipe of recipes) {
+// 		if (recipe.ingredients.find(ingredient => ingredient.foodstuff.name === item.foodstuff.name && ingredient.quantity <= item.quantity)) { set.add(recipe)}
+
+// 	}
+
+// 	return set
+	
+// })
