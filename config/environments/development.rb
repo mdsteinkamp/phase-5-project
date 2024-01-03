7fee21acc268bdbe87de92aab7bfedd3b288dd72
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-03 08:27:17 -0800

    added Rails gmail config to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,3 +69,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:         'smtp.gmail.com',
+  port:            587,
+  domain:          'example.com',
+  user_name:       '<username>',
+  password:        '<password>',
+  authentication:  'plain',
+  enable_starttls: true,
+  open_timeout:    5,
+  read_timeout:    5 }
