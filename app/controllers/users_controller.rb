bcf2f7f101964b0d50349ddb3bcf08d7e9d39bd3
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-03 08:36:54 -0800

    updated User#create to new & then send welcome_email if saved & added template to welcome_email.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,11 @@ class UsersController < ApplicationController
   end
 
   def create 
-    user = User.create!(user_params)
-    session[:user_id] = user.id
-    render json: user, status: :created
+    @user = User.new(user_params)
+    if @user.save!
+      session[:user_id] = user.id
+      UserMailer.with(user: current_user).welcome_email.deliver_now
+      render json: user, status: :created
   end
 
   private
