862fc51062a9277ac1366e0e074dc7286b142d02
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-03 09:03:08 -0800

    users_controller updated, Signup will work but not sending welcome_email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save!
-      session[:user_id] = user.id
-      UserMailer.with(user: current_user).welcome_email.deliver_now
-      render json: user, status: :created
+      session[:user_id] = @user.id
+      UserMailer.with(user: @user).welcome_email.deliver_now
+      render json: @user, status: :created
+    end
   end
 
   private
