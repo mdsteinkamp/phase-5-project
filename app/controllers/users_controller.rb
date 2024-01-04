4fd6ad6b5472c4f17a8ac23bc6e12562e0b8eee5
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-04 15:32:56 -0800

    fallback to top of routes for deploy attempt & no else in users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
 
   def create 
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       session[:user_id] = @user.id
       UserMailer.with(user: @user).welcome_email.deliver_now
       render json: @user, status: :created
-    else
-      render json: { errors: ["Error on creation"] }, status: :unprocessable_entity
+    # else
+    #   render json: { errors: ["Error on creation"] }, status: :unprocessable_entity
     end
   end
 
