3fbc1cb24425a7aff638bf32b860c13d48844e88
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-04 15:53:34 -0800

    reactivating actionmailer settings in production.rb and Usermailer in users_cont #create

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,15 +87,14 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:         'smtp.gmail.com',
-  #   port:            587,
-  #   domain:          'https://pantry-app-032x.onrender.com/',
-  #   user_name:       ENV["GMAIL_USERNAME"],
-  #   password:        ENV["GMAIL_PASSWORD"],
-  #   authentication:  'plain',
-  #   enable_starttls: true,
-  #   open_timeout:    5,
-  #   read_timeout:    5 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    user_name:       ENV["GMAIL_USERNAME"],
+    password:        ENV["GMAIL_PASSWORD"],
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 end
