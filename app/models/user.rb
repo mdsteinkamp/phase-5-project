85a59c4228ccc7f46f01bc518855ec78f4223a9d
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-01-23 17:06:14 -0800

    user is validating email format and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+
   has_many :pantry_items
   has_many :foodstuffs, through: :pantry_items
 end
